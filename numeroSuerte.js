5e586094ff595a0f0cea89f2ac9e9994dee4809f
Your Name <GreenCodeEE>
2021-03-27 18:59:48 -0600

    tarea

---
+++ numeroSuerte.js
@@ -0,0 +1,5 @@
+const PromptSync = require("prompt-sync");
+const prompt = require ('prompt-sync')();
+console.log("Hola, hoy calcularemos tu numero de la suerte");
+var año = prompt ("¿Qué año naciste?");
+console.log (año); 
